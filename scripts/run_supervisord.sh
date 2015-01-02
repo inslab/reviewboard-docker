4eea5b102c9362ceafc58de41d90def048ccf23b
inslab.dev <inslab.dev@gmail.com>
2015-01-02 16:35:12 +0900

    change reviewboard file copy location

--- scripts/run_supervisord.sh
+++ scripts/run_supervisord.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # fix database permissions
+if [ ! -f /srv/reviews.local/data/reviewboard ]
+then
+    cp -f /root/reviewboard /srv/reviews.local/data/reviewboard
+fi
+
 chown -R www-data:www-data /srv/reviews.local/data
 rb-site upgrade /srv/reviews.local
 
-if [ ! -f /srv/reviews.local/data/reviewboard ]
-then
-    cp -f /tmp/reviewboard /srv/reviews.local/data/reviewboard
-fi
-
 # run supervisor
 /usr/bin/supervisord -n
+
